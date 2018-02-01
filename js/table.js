8de55d08bfe9f639659a1159a792f5e1da3c9f5f
DAVID CERINO <DAVID.CERINO.U@GMAIL.COM>
2018-01-31 20:36:13 -0800

    final form

--- js/table.js
+++ js/table.js
@@ -4,7 +4,8 @@
 
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 var locations = [];
-
+var salesTable = document.getElementById('salesTable');
+var addNewStore = document.getElementById('addNewStore');
 
 //constructor
 function newStore(name, minCustomers, maxCustomers, avgCookies) {
@@ -23,6 +24,20 @@ function newStore(name, minCustomers, maxCustomers, avgCookies) {
   locations.push(this);
 }
 
+newStore.prototype.avgCustPerHr = function() {
+  for (var i = 0; i < hours.length; i++) {
+    var singleHourCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
+    this.averageCustomersPerHour.push(singleHourCustomers);
+  }
+};
+newStore.prototype.cookiesPerCustomer = function() {
+  for (var i = 0; i < hours.length; i++) {
+    var singleHourCookies = Math.ceil(this.averageCustomersPerHour[i] * this.avgCookies);
+    this.averageCookiesPerHour.push(singleHourCookies);
+    this.totalCookiesPerDay += singleHourCookies;
+  }
+};
+
 newStore.prototype.render = function() {
   var trRowName = document.createElement('tr');
   var tdStore = document.createElement('td');
@@ -38,31 +53,18 @@ newStore.prototype.render = function() {
   trRowName.appendChild(tdTotal);
   salesTable.appendChild(trRowName);
 };
-newStore.prototype.avgCustPerHr = function() {
-  for (var i = 0; i < hours.length; i++) {
-    var singleHourCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
-    this.averageCustomersPerHour.push(singleHourCustomers);
-  }
-};
-newStore.prototype.cookiesPerCustomer = function() {
-  for (var i = 0; i < hours.length; i++) {
-    var singleHourCookies = Math.ceil(this.averageCustomersPerHour[i] * this.avgCookies);
-    this.averageCookiesPerHour.push(singleHourCookies);
-    this.totalCookiesPerDay += singleHourCookies;
-  }
-};
+
 //create table header
 function makeHeaderRow() {
   var tableRow = document.createElement('tr');
   var thElement = document.createElement('th');
-  thElement.textContent = null;
+  thElement.textContent = 'Location';
   salesTable.appendChild(tableRow);
   tableRow.appendChild(thElement);
   for (var i = 0; i < hours.length; i++) {
     thElement = document.createElement('th');
     thElement.textContent = hours[i];
     tableRow.appendChild(thElement);
-
   }
   thElement = document.createElement('th');
   thElement.textContent = 'Location Total';
@@ -70,6 +72,7 @@ function makeHeaderRow() {
   console.log(salesTable);
   salesTable.appendChild(tableRow);
 }
+
 function renderStore() {
   for (var i = 0; i < locations.length; i++) {
     locations[i].render();
@@ -102,8 +105,37 @@ new newStore('Alki', 2, 16, 4.6);
 new newStore('SeaTac Airport', 3, 24, 1.2);
 new newStore('Capitol Hill', 20, 38, 2.3);
 
-
-
 makeHeaderRow();
 renderStore();
 makeFooterRow();
+
+//form setup
+function submitNewStore(event) {
+  event.preventDefault(); //prevents page re-load on a submittal
+  console.log('"submit" even executed');
+  if(!event.target.name.value || !event.target.minCustomers.value || !event.target.maxCustomers.value || !event.target.avgCookies.value) {
+    return alert('Please fill in ALL boxes');
+  }
+  var name = event.target.name.value;
+  console.log(event.target.name);
+  var minCustomers = parseInt(event.target.minCustomers.value);
+  var maxCustomers = parseInt(event.target.maxCustomers.value);
+  var avgCookies = parseFloat(event.target.avgCookies.value);
+
+  event.target.name = null;
+  event.target.minCustomers = null;
+  event.target.maxCustomers = null;
+  event.target.avgCookies = null;
+
+  var newStoreInput = new newStore(name, minCustomers, maxCustomers, avgCookies);
+  
+  salesTable.textContent = null;//keeps the table from replicating over and over!
+
+  makeHeaderRow();
+  renderStore();
+  makeFooterRow();
+  console.log('new store submitted!')
+}
+
+addNewStore.addEventListener('submit', submitNewStore);
+
